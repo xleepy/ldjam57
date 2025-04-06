7c4a6f949a2591533cbce6ca0f93a1fc9945531b
Radmir Gurkov <ragux0@gmail.com>
2025-04-06 15:29:40 +0200

    Fix couches selection

--- scripts/couch_scene.gd
+++ scripts/couch_scene.gd
@@ -1,8 +1,12 @@
-extends StaticBody2D
+extends InteractableObject
 
-@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation: AnimatedSprite2D = $Green
 @export_enum("red", "green", "purple") var chosen_animation: String = "green"
 
 func _ready() -> void:
 	if animation and chosen_animation:
 		animation.play(chosen_animation)
+
+
+func interact() -> void:
+	player.sit()
