bfc4c558e4de727e7b343f593a9c6b00ae1fe977
Radmir Gurkov <ragux0@gmail.com>
2025-04-06 16:57:47 +0200

    Fix invisible walls issue

--- scripts/room.gd
+++ scripts/room.gd
@@ -3,12 +3,13 @@ extends Node2D
 @export_enum("left_wall", "right_wall", "hallway") var room_variant = "hallway"
 
 var room_visibility_by_variant: Dictionary = {
-	'left_wall': "LeftWall",
-	"right_wall": "RightWall"
+	'left_wall': {"wall": "LeftWall", "door": "RightWall"},
+	"right_wall": {"wall": "RightWall", "door": "LeftWall"}
 }
 
 func _ready() -> void:
 	var visibility = room_visibility_by_variant[room_variant]
 	if visibility:
-		get_node(visibility).visible = true	
+		get_node(visibility['wall']).visible = true	
+		get_node(visibility['door']).queue_free()
 		
